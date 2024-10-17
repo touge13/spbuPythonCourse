ee56f018a66103727be9bb6427eb73363463f75c
touge13 <grudinin.miha@mail.ru>
2024-10-17 14:51:23 +0300

    task3 completed

--- project/generators/rgba.py
+++ project/generators/rgba.py
@@ -1,11 +1,13 @@
-def get_rgba_element(i):
-    rgba_generator = (
+def rgba_generator():
+    return (
         (r, g, b, a)
         for r in range(256)
         for g in range(256)
         for b in range(256)
         for a in range(0, 101, 2)
     )
-    for idx, rgba in enumerate(rgba_generator):
+
+def get_rgba_element(i):
+    for idx, rgba in enumerate(rgba_generator()):
         if idx == i:
             return rgba
